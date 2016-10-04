e0c4b251278a0a483f78fa2bb8fc387bb7a2e3a6
Black-and-white-Friday13 <vFriday13v@gmail.com>
2016-10-04 16:13:30 +0300

    new change

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Created by Fridayl3 on 9/13/2016.
- */
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int[] a = new int[20];
-        for (int i = 0; i < a.length; i++){
-            a[i] = Integer.parseInt(reader.readLine());
-                }
         int[] b = new int[10];
         int[] c = new int[10];
+        int j = 0;
+        int k = 0;
         for (int i = 0; i < a.length; i++){
-            if (b.length > i)
-                b[i] = a[i];
-            else
-                c[i] = a[i];
-            System.out.println(c[i]);
+            a[i] = Integer.parseInt(reader.readLine());
+        }
+        for (int i = 0; i < a.length; i++){
+            if (i < b.length) {
+                b[j] = a[i];
+                ++j;
+            }
+            else {
+                c[k] = a[i];
+                ++k;
+            }
+        }
+        for (int i : c){
+            System.out.println(i);
         }
 
     }
